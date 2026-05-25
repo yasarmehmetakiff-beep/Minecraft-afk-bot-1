0c1f0f4c9dcd369fc6f4ca92f21a24bb196cd7c8
yasarmehmetakiff-beep <yasarmehmetakiff@gmail.com>
2026-05-25 12:27:49 +0300

    Update bot-authme-first.js

--- bot-authme-first.js
+++ bot-authme-first.js
@@ -4,15 +4,15 @@ const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
 // Configuration - Edit these values for your server
 const config = {
   server: {
-    host: 'localhost', // Change to your server IP
-    port: 25565,
-    version: '1.20.4' // Change to your server version
+    host: 'SMP-26sincik.aternos.me', // Change to your server IP
+    port: 59592,
+    version: '1.21.11' // Change to your server version
   },
   bot: {
     username: 'AFKBot', // Change to your desired bot name
     auth: 'offline', // 'offline', 'microsoft', or 'mojang'
     password: '', // Minecraft account password (if using premium auth)
-    authmePassword: 'change_this_password' // AuthMe password for /register and /login
+    authmePassword: '290502' // AuthMe password for /register and /login
   },
   serverCommands: {
     enabled: true,
